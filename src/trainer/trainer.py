c88ac05ce44cfa3e10d4302b9786d14a708ae4af
Jun-Min Lee <ljm56897@gmail.com>
2024-06-11 00:18:46 +0900

    feat: combine progress bar function

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -15,7 +15,7 @@ from torch import distributed as dist
 from tools import TrainingLogger
 from trainer.build import get_model, get_data_loader
 from utils import TQDM, RANK, LOGGER, colorstr, init_seeds
-from utils.training_utils import init_train_progress_bar
+from utils.training_utils import init_progress_bar
 from utils.filesys_utils import *
 
 
@@ -163,7 +163,7 @@ class Trainer:
 
         # init progress bar
         if RANK in (-1, 0):
-            pbar = init_train_progress_bar(train_loader, self.is_rank_zero, ['MSE Loss'], nb)
+            pbar = init_progress_bar(train_loader, self.is_rank_zero, ['MSE Loss'], nb)
 
         for i, (x, _) in pbar:
             self.train_cur_step += 1
@@ -198,19 +198,11 @@ class Trainer:
             epoch: int,
             is_training_now=True
         ):
-        def _init_headline():
-            header = tuple(['Epoch'] + ['MSE Loss'])
-            LOGGER.info(('\n' + '%15s' * 2) % header)
-
-        def _get_val_pbar(dloader, nb):
-            _init_headline()
-            return TQDM(enumerate(dloader), total=nb)
-        
         with torch.no_grad():
             if self.is_rank_zero:
                 val_loader = self.dataloaders[phase]
                 nb = len(val_loader)
-                pbar = _get_val_pbar(val_loader, nb)
+                pbar = init_progress_bar(val_loader, self.is_rank_zero, ['MSE Loss'], nb)
 
                 self.model.eval()
 
